88b62f187a3ada28a7a8d61e73293c271b90d469
OlegKolchin <commshepard@gmail.com>
2021-08-03 19:56:34 +0500

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,7 +5,17 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
 public class SqlRuParse {
+    public static String postReader(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Element el = doc.selectFirst(".msgBody");
+        Element body = el.parent().child(1);
+        Element date = el.parent().parent().child(2).child(0);
+        return String.format("%s%n%s", body.text(), date.text().split("\\[")[0]);
+    }
+
     public static void main(String[] args) throws Exception {
         int index = 1;
         while (index <= 5) {
@@ -14,9 +24,11 @@ public class SqlRuParse {
             for (Element td : row) {
                 Element date = td.parent().child(5);
                 Element href = td.child(0);
-                System.out.println(href.attr("href"));
+                String url = href.attr("href");
+                System.out.println(url);
                 System.out.println(href.text());
                 System.out.println(date.text());
+                System.out.println(postReader(url));
             }
             index++;
         }
