24804d1aee3dfcb268d73db6db7d8c1c8413c12f
OlegKolchin <commshepard@gmail.com>
2021-08-06 07:26:33 +0500

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,10 +4,55 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
+import ru.job4j.grabber.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    @Override
+    public List<Post> list(String link) throws IOException, ParseException {
+        List<Post> rsl = new ArrayList<>();
+        int index = 1;
+        while (index <= 5) {
+            Document doc = Jsoup.connect(link + index).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                String url = href.attr("href");
+                String title = href.text();
+                String desc = postReader(url);
+                String[] temp = desc.split("\\n");
+                int id = 0;
+                LocalDateTime created = dateTimeParser.parse(temp[temp.length - 1]);
+                rsl.add(new Post(id++, title, url, desc, created));
+            }
+            index++;
+        }
+        return rsl;
+    }
+
+    @Override
+    public Post detail(String link) throws IOException, ParseException {
+        Document doc = Jsoup.connect(link).get();
+        String name = doc.selectFirst(".messageHeader").text();
+        String desc = postReader(link);
+        String[] temp = desc.split("\\n");
+        LocalDateTime created = dateTimeParser.parse(temp[temp.length - 1]);
+        return new Post(0, name, link, desc, created);
+    }
+
     public static String postReader(String url) throws IOException {
         Document doc = Jsoup.connect(url).get();
         Element el = doc.selectFirst(".msgBody");
